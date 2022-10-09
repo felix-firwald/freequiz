72b69aec022cc6cf7f04af654d5be4d277a44aa2
felix-firwald <felixfirwald@gmail.com>
2022-10-09 13:03:39 +0300

    bugs fixed

--- Freequiz/core/views.py
+++ Freequiz/core/views.py
@@ -5,5 +5,5 @@ def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
 
-def internal_server_error(request, exception):
+def internal_server_error(request):
     return render(request, 'core/500.html', {'path': request.path}, status=500)
